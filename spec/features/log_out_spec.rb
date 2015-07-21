b15054d56153956d9bb6d5ed97acc3da6e73dd3b
AleksandrKudashkin <akudashkin@yandex.ru>
2015-07-21 14:49:32 +0700

    Add registration and logout

---
+++ spec/features/log_out_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+feature 'User log out', '
+  In order to be able end my visit to this site
+  As an User
+  I want to be able to log out
+
+' do
+  given(:user) { create(:user) }
+
+  scenario 'Registered user tries to sign in' do
+    sign_in(user)
+
+    visit root_path
+    click_link 'Log out'
+
+    expect(page).to have_content 'Signed out successfully.'
+    expect(current_path).to eq root_path
+  end
+ 
+end
---
+++ spec/features/sign_up_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+feature 'User signs up', '
+  In order to be able to ask a question
+  As an User
+  I want to be able to sign up to site
+
+' do
+  scenario 'Registered user tries to sign in' do
+    visit new_user_registration_path
+    fill_in 'Email', with: 'iam@theuser.com'
+    fill_in 'Password', with: '12345678'
+    fill_in 'Password confirmation', with: '12345678'
+    click_on 'Register me!'
+
+
+    expect(page).to have_content 'Welcome! You have signed up successfully.'
+    expect(current_path).to eq root_path
+  end
+end
