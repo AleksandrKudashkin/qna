69d4598aac5bdeacd3f5396c0bedf1e671c393f8
akudashkin <akudashkin@yandex.ru>
2016-10-24 03:03:37 -0400

    Add capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,105 @@
+# config valid only for current version of Capistrano
+lock '3.6.1'
+
+set :application, 'qna'
+set :repo_url, 'git@github.com:AleksandrKudashkin/qna.git'
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, "/home/deployer/qna"
+set :deploy_user, 'deployer'
+
+# Default value for :linked_files is []
+append :linked_files, 'config/database.yml', 'config/private_pub.yml', 'config/private_pub_thin.yml', '.env', 'config/production.sphinx.conf'
+
+# Default value for linked_dirs is []
+append :linked_dirs, 'bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'vendor/bundle', 'public/uploads', 'db/sphinx'
+
+namespace :deploy do
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt') 
+    end
+  end
+
+  after :publishing, :restart
+end
+
+# namespace :thinking_sphinx do
+#   desc 'Start searchd'
+#   task :start do
+#     on roles(:app) do
+#       within current_path do
+#         with rails_env: fetch(:rails_env) do
+#           execute :bundle, "exec rake ts:start"
+#         end
+#       end
+#     end
+#   end
+
+#   task :stop do
+#     on roles(:app) do
+#       within current_path do
+#         with rails_env: fetch(:rails_env) do
+#           execute :bundle, "exec rake ts:stop"
+#         end
+#       end
+#     end
+#   end
+
+  
+#   task :restart do
+#     on roles(:app) do
+#       within current_path do
+#         with rails_env: fetch(:rails_env) do
+#           execute :bundle, "exec rake ts:restart"
+#         end
+#       end
+#     end
+#   end
+
+#   task :rebuild do
+#     on roles(:app) do
+#       within current_path do
+#         with rails_env: fetch(:rails_env) do
+#           execute :bundle, "exec rake ts:rebuild"
+#         end
+#       end
+#     end
+#   end
+# end
+
+namespace :private_pub do 
+  desc 'Start private_pub server'
+  task :start do
+    on roles(:app) do
+      within current_path do
+        with rails_env: fetch(:rails_env) do
+          execute :bundle, "exec thin -C config/private_pub_thin.yml start"
+        end
+      end
+    end
+  end
+
+  task :stop do
+    on roles(:app) do
+      within current_path do
+        with rails_env: fetch(:rails_env) do
+          execute :bundle, "exec thin -C config/private_pub_thin.yml stop"
+        end
+      end
+    end
+  end
+
+  task :restart do
+    on roles(:app) do
+      within current_path do
+        with rails_env: fetch(:rails_env) do
+          execute :bundle, "exec thin -C config/private_pub_thin.yml restart"
+        end
+      end
+    end
+  end
+end
+
+after 'deploy:restart', 'private_pub:restart'
