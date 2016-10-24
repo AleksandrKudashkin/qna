0aa12b5d3e6b9cc20f3dbbd55f1bf303f9aabbec
akudashkin <akudashkin@yandex.ru>
2016-10-24 19:35:54 -0400

    Add confirmable to Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ActiveRecord::Base
   has_many :authorizations, dependent: :destroy
   has_many :subscriptions, dependent: :destroy
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  # :lockable, :timeoutable
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, omniauth_providers: [:facebook, :twitter]
 
--- config/deploy.rb
+++ config/deploy.rb
@@ -12,20 +12,21 @@ set :deploy_user, 'deployer'
 append :linked_files, 'config/database.yml', 'config/private_pub.yml', 'config/private_pub_thin.yml', '.env', 'config/production.sphinx.conf'
 
 # Default value for linked_dirs is []
-append :linked_dirs, 'bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'vendor/bundle', 'public/uploads', 'db/sphinx'
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'vendor/bundle', 'public/uploads', 'db/sphinx'
+# 'bin'
 
 namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      execute :touch, release_path.join('tmp/restart.txt') 
+      execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
   after :publishing, :restart
 end
 
-namespace :private_pub do 
+namespace :private_pub do
   desc 'Start private_pub server'
   task :start do
     on roles(:app) do
