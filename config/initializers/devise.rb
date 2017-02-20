f2d39e32901a858c9e008bb2f6848fb5e13dc2fc
AleksandrKudashkin <alex@kudashkin.pro>
2017-02-19 19:12:04 -0500

    Correct domain name for ActionMailer

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'notifier@kudashkin.pro'
+  config.mailer_sender = 'notifier@qna.kudashkin.pro'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
